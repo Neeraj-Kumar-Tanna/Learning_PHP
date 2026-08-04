deba171762a65303e9906689eea6fe208bcaca50
Neeraj kumar <neeraj772006@gmail.com>
2026-08-04 23:11:31 +0530

    vaiables

--- index.php
+++ index.php
@@ -1,21 +1,19 @@
 <?php
-    echo"i am neeraj kumar tanna <br>";
-    echo"i am studying in bvrit college";
-    
-?>
+    //Variables . 
+    $name = "neeraj kumar tanna <br>";
+    echo $name;
+    $age = 20;
+    echo "age is : {$age} <br>";
+    $cgpa = 8.06;
+    echo "cgpa is : {$cgpa} <br>";
+    $male = true;
+    if($male){
+        echo "male";
+    }
+    else{
+        echo "female";
+    }
+    echo "<br> is male : {$male}";
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Learning php</title>
-</head>
-<body>
-    <h1>i am studying in bvrit college</h1>
-    <p>how are you brother</p>
-    <button>i am ok brother</button>
-    <span>hii</span>
-    <span>helloooo</span>
-</body>
-</html>
+
+?>
