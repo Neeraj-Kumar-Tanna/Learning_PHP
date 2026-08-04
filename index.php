cd9e93f83178550f35edbc4af91e89a74d4ff973
Neeraj kumar <neeraj772006@gmail.com>
2026-08-04 23:05:31 +0530

    printing the statements

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
 <?php
-    echo"i am neeraj kumar tanna ";
+    echo"i am neeraj kumar tanna <br>";
     echo"i am studying in bvrit college";
+    
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Learning php</title>
+</head>
+<body>
+    <h1>i am studying in bvrit college</h1>
+    <p>how are you brother</p>
+    <button>i am ok brother</button>
+    <span>hii</span>
+    <span>helloooo</span>
+</body>
+</html>
