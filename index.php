8145b11b8189bd32f5ed38698b667271b84cb4c6
Neeraj kumar <neeraj772006@gmail.com>
2026-08-05 07:40:18 +0530

    conditional statements

--- index.php
+++ index.php
@@ -1,30 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>learning php</title>
-</head>
-<body>
-    <form action="index.php" method="post">
-        <label >X : </label>
-        <input type="text" name="x">
-        <br>
-        <input type="submit" value="calculate">
-        <br>
-    </form>
-</body>
-</html>
-
 <?php
-    //GET and POST
-    $x = $_POST["x"];
-    $circumference = round(2*pi()*$x , 2);
-    $area = round(pi()*pow($x , 2) , 2);
-    $volume = round(4/3*pi()*pow($x, 3) , 2);
+    //conditional statements....
 
-    echo "circumference : {$circumference} <br>";
-    echo "area : {$area} <br>";
-    echo "volume : {$volume} <br>";
-    
+    $age = 1000;
+
+    if($age >= 18){
+        echo "you can vote";
+    }
+    else if($age == 0){
+        echo "just born";
+    }
+    else if($age < 0){
+        echo "invalid age ";
+    }
+    else{
+        echo "teen age";
+    }
 ?>
